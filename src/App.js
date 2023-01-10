882f3ab9f0ac6015896a76b2cc1e6cb724e26ccb
Daniel Zhang <alex_6327@hotmail.com>
2023-01-10 11:49:15 -0800

    fix loading bugs

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ import ChartView from "./components/ChartView";
 
 const App = () => {
   const [result, setResult] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const parseCsv = () => {
     return new Promise((resolve, reject) => {
       Papa.parse(csvFile, {
         download: true,
         complete: function (parsedResult) {
+          console.log("papaparse complete");
           resolve(parsedResult.data);
         },
         error: function (error) {
@@ -26,37 +28,45 @@ const App = () => {
   };
 
   useEffect(() => {
+    console.log("useEffect in app.js");
     parseCsv()
       .then((data) => {
+        console.log("useEffect promise then");
         setResult(data);
+        setLoading(false);
       })
       .catch((error) => {
         console.error(error);
+        setLoading(false);
       });
   }, []);
 
-  if (result.length === 0) {
-    return null;
-    console.log("Error: result length is 0");
+  // if (result.length === 0) {
+  //   console.log("Error: result length is 0");
+  //   return null;
+  // }
+  if (loading) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div className="container">
+        <Header />
+        <Tabs
+          defaultActiveKey="tableView"
+          id="fill-tab-example"
+          className="mb-2"
+          fill
+        >
+          <Tab eventKey="tableView" title="Table View">
+            <TableView data={result} />
+          </Tab>
+          <Tab eventKey="chartView" title="Chart View">
+            <ChartView data={result} />
+          </Tab>
+        </Tabs>
+      </div>
+    );
   }
-  return (
-    <div className="container">
-      <Header />
-      <Tabs
-        defaultActiveKey="tableView"
-        id="fill-tab-example"
-        className="mb-2"
-        fill
-      >
-        <Tab eventKey="tableView" title="Table View">
-          <TableView data={result} />
-        </Tab>
-        <Tab eventKey="chartView" title="Chart View">
-          <ChartView data={result} />
-        </Tab>
-      </Tabs>
-    </div>
-  );
 };
 
 export default App;
